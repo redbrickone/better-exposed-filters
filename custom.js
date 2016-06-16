c2cba0d996ec7c0cf5c50523f21309dd97965a45
Chris Wright <redbrickone@live.com>
2016-06-16 12:30:48 -0500

    fun things

---
+++ custom.js
@@ -0,0 +1,17 @@
+var link = document.createElement( 'link' );
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = window.location.search.match( /print-pdf/gi ) ? 'css/print/pdf.css' : 'css/print/paper.css';
+			document.getElementsByTagName( 'head' )[0].appendChild( link );
+
+
+Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+
+				transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+				// Optional reveal.js plugins
+			});
